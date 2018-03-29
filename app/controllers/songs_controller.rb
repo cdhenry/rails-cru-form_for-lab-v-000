cf77cf11ee10d2ff9f52ab62954b049af7c570d3
Chris Henry <cdhenry@gmail.com>
2018-03-29 23:22:19 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create(song_params(:name))
+    @song = Song.create(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
